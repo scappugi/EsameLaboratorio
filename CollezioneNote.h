91fd46f9ef9f8038369ae3d42868254c46ac4d18
Simone <simone.cappugi@stud.unifi.it>
2023-02-01 10:53:50 +0100

    COMMIT: fatti miglioramenti per facilitare lettoura metodi in CollezioneNote.h

--- CollezioneNote.h
+++ CollezioneNote.h
@@ -18,32 +18,37 @@ public:
     explicit CollezioneNote(const string& nomeLista): nomeLista(nomeLista){ //costruttore che assegna un nome alla list
       }
 
-     ~CollezioneNote() override = default;
+     ~CollezioneNote() override = default; //distruttore
 
-    ///metodi gestione lista
+    ///metodi per aggiungere
     virtual void AddNoteToList(Nota *nota); //NON AGGIUNGERE NOTE CON LO STESSO TITOLO, ESSO DEVE ESSERE UNIVOCO
+
+
+    ///metodi per rimuovere
     virtual bool removeToList(Nota *nota);
     virtual bool removeAndDestroyNote(Nota *nota);
+
+
+    ///metodi get e set
     void setNomeLista(const string &name);
     bool bloccaNota(Nota *nota); //ritorna 1 se bloccata con successo altrimenti 0
     bool sbloccaNota(Nota *nota); //permette di togliere il blocco messo da bloccaNota
     void bloccaTutteLeNote();//pone tutte le note su bloccato
     void aumentaPrioritaNota(Nota *nota);
     void diminuisciPrioritaNota(Nota *nota);
-    void modificaNota(Nota *nota, const string &newTitle); //vuole andare a cambiare il titolo della nota
-    Nota *getNota(const string &nomeNota); //restituisce la prima nota con quel nome
     string getNomeLista();
+    Nota *getNota(const string &nomeNota); //restituisce la prima nota con quel nome
+
+
+    ///metodi per modificare e ricercare
+    void modificaNota(Nota *nota, const string &newTitle); //vuole andare a cambiare il titolo della nota
     void stampaNote();
     bool cercaNote(const Nota *nota);
-    ///fine
 
     ///metodi per pattern observer
     void subscribe(Observer *o) override;
-
     void unsubscribe(Observer *o) override;
-
     void notify() override;
-    ///fine
 
 
 
